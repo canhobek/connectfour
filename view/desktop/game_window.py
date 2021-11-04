d26767df9924fe5fbb8d507c21fbd1efe85a5031
onuremir <emironur22@gmail.com>
2021-11-04 23:28:51 +0300

    -add player iter for tiles

--- view/desktop/game_window.py
+++ view/desktop/game_window.py
@@ -1,4 +1,3 @@
-
 from PyQt5.QtCore import QSize, Qt, QRect
 from PyQt5.QtGui import QPaintEvent, QMouseEvent, QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow
@@ -6,15 +5,16 @@ from PyQt5.QtWidgets import QMainWindow
 from model.board_model_listener import BoardModelListener
 from controller.desktop.mouse_controller import MouseController
 from model.tile import Tile
+from model.player import CircularPlayerIterator
 
 
 class GameWindow(QMainWindow, BoardModelListener):
     WIDTH = 700
     HEIGHT = 600
     COL_WIDTH = 100
-    TILE_DIAMETER = 90
+    TILE_DIAMETER = 100
 
-    def __init__(self, board, *players, title="Connect Four"):
+    def __init__(self, board, players, title="Connect Four"):
         super(GameWindow, self).__init__()
         self.setWindowTitle(title)
         self.setFixedSize(QSize(GameWindow.WIDTH, GameWindow.HEIGHT))
@@ -22,8 +22,9 @@ class GameWindow(QMainWindow, BoardModelListener):
 
         self._board.addListener(self)
 
-        self.mouseController = MouseController(board, self)
+        self._player_iter = CircularPlayerIterator(players)
 
+        self.mouseController = MouseController(board, self)
 
     def paintEvent(self, a0: QPaintEvent) -> None:
         painter = QPainter(self)
@@ -31,7 +32,18 @@ class GameWindow(QMainWindow, BoardModelListener):
         for x in range(GameWindow.COL_WIDTH, GameWindow.WIDTH, GameWindow.COL_WIDTH):
             painter.drawLine(x, 0, x, GameWindow.HEIGHT)
 
-        tile_list = []
+        self._draw_board(painter)
+
+    def mouseDoubleClickEvent(self, mouseEvent: QMouseEvent) -> None:
+        if mouseEvent.button() == Qt.LeftButton:
+            self.mouseController.mouseDoubleClickEvent(mouseEvent, next(self._player_iter).tile)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            # self.pressPos = event.pos()
+            pass
+
+    def _draw_board(self, painter):
         r = 0
         for row in self._board:
             c = 0
@@ -42,61 +54,19 @@ class GameWindow(QMainWindow, BoardModelListener):
                     continue
 
                 if elem == Tile.RED:
-                    painter.setPen(QColor(255, 0, 0))
+                    painter.setBrush(QColor(183, 0, 0))
                 elif elem == Tile.BLUE:
-                   painter.setPen(QColor(0, 0, 255))
+                    painter.setBrush(QColor(0, 0, 160))
 
-                tile_list.append(QRect(c, GameWindow.HEIGHT - GameWindow.TILE_DIAMETER + r,
-                                    GameWindow.TILE_DIAMETER, GameWindow.TILE_DIAMETER))
+                painter.drawEllipse(QRect(c, GameWindow.HEIGHT - GameWindow.TILE_DIAMETER + r,
+                                          GameWindow.TILE_DIAMETER, GameWindow.TILE_DIAMETER))
 
                 c += GameWindow.COL_WIDTH
             r -= GameWindow.COL_WIDTH
-
-
-        for rect in tile_list:
-            painter.drawEllipse(rect)
-
-    def mouseDoubleClickEvent(self, mouseEvent: QMouseEvent) -> None:
-        if mouseEvent.button() == Qt.LeftButton:
-            self.mouseController.mouseDoubleClickEvent(mouseEvent, Tile.RED)
-
-    def mousePressEvent(self, event):
-        if event.button() == Qt.RightButton:
-            #self.pressPos = event.pos()
-            pass
-
-    def _draw_board(self):
-        """
-        painter = QPainter()
-        r = 0
-        for row in self._board:
-            c = 0
-
-            for elem in row:
-                if elem == Tile.EMPTY:
-                    c += GameWindow.COL_WIDTH
-                    continue
-
-                #if elem == Tile.RED:
-                #    painter.setPen(QColor(255, 0, 0))
-                #elif elem == Tile.BLUE:
-                #    painter.setPen(QColor(0, 0, 255))
-
-                painter.drawEllipse(c, GameWindow.HEIGHT - GameWindow.TILE_DIAMETER + r,
-                                    GameWindow.TILE_DIAMETER, GameWindow.TILE_DIAMETER)
-
-                c += GameWindow.COL_WIDTH
-            r += GameWindow.COL_WIDTH
-        """
 
     def board_changed(self):
         """
         abstract method imp. model listener
         """
-        print("board changedmiş")
-        GameWindow.R = 0
-
-        self.update()
-        #self.repaint()
-
-
+        print("board changed")
+        self.update()  # trigger paintEvent
